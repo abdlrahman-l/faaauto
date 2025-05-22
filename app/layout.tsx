128a63b8be0b0a6c099677f4f7aeb8c572653aba
abdlrahman-l <abdulrahmaninformatics@gmail.com>
2025-05-22 17:16:03 +0700

    improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { title } from "process";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,16 +14,37 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: 'Pinjaman BPKB Mobil & Motor | ACC, Adira, BFI, Buana Finance, Cimb Niaga, Clipan Panin',
+const siteConfig = {
+  title: 'FAAAuto | Pinjaman BPKB Mobil & Motor | ACC, Adira, BFI, Buana Finance, Cimb Niaga, Clipan Panin',
   description: 'Ajukan pinjaman jaminan BPKB motor/mobil proses cepat. ACC, Adira, BFI, Buana Finance, Cimb Niaga, Clipan Panin. Terpercaya dan aman!',
+}
+
+export const metadata: Metadata = {
+  title: siteConfig.title,
+  description: siteConfig.description,
   robots: {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: '/favicon.ico'
+  },
   openGraph: {
-    images: ['/image/faauto-banner-1.webp']
-  }
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: 'FAAAuto',
+    locale: "id_ID",
+    type: "website",
+    images: [
+      {
+        url: "/image/og-banner.webp",
+        width: 1200,
+        height: 630,
+        alt: "Pinjaman Dana Cepat",
+      },
+    ]
+  },
+
 };
 
 export default function RootLayout({
